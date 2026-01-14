cab9ebd3a35abace8343496a87d0e13d30f5b715
na709 <naa2203dz@gmail.com>
2026-01-15 00:01:50 +0700

    Revert "update v2"
    This reverts commit d3e0bdb95603d050ec201b1cf345e180c399ac04.

--- app/src/main/java/com/example/techz/model/Cart.kt
+++ app/src/main/java/com/example/techz/model/Cart.kt
@@ -1,6 +1,8 @@
 package com.example.techz.model
+///
 import com.google.gson.annotations.SerializedName
 
+// Request gửi lên khi Thêm/Sửa/Xóa giỏ hàng
 data class CartRequest(
     @SerializedName("id_khach_hang")
     val userId: Int,
@@ -11,7 +13,8 @@ data class CartRequest(
     @SerializedName("so_luong")
     val quantity: Int
 )
-
+//
+// Item hiển thị trong danh sách giỏ hàng
 data class CartItem(
     @SerializedName("product")
     val product: Product,
